2fd0116029b64f2647cf256dae3e1f49c203c89f
NOESYU <86655940+NOESYU@users.noreply.github.com>
2022-02-23 17:26:26 +0900

    20220223_3

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public Rigidbody playerRb;
+    public float speed = 8f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
