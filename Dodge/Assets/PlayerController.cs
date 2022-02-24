c89fac821c6455ba3b9c5504a62c09ee24fe6106
dprnd <ssunuu114@gmail.com>
2022-02-24 17:32:15 +0900

    20220224_1

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRb;
     public float speed = 8f;
-    
+    public GameObject my;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+    }
+
+    void DirectInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerRb.AddForce(0f, 0f, speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerRb.AddForce(0f, 0f, -speed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerRb.AddForce(speed, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRb.AddForce(-speed, 0f, 0f);
+        }
+    }
+
+    void Die()
+    {
+        my.SetActive(false);
     }
 }
